9a13122f4b9ce439e97a710e9d86b8a1ed55b7f1
Micoder-dev <micoder.com@gmail.com>
2022-07-24 19:25:12 +0530

    alertDialog with edtText added

--- app/src/main/java/com/example/masters/MainActivity.java
+++ app/src/main/java/com/example/masters/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.masters;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -29,9 +33,38 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "fab", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this, "fab", Toast.LENGTH_SHORT).show();
+
+                displayCreateCategoryDialog();
+
             }
         });
 
     }
+
+    private void displayCreateCategoryDialog() {
+
+        String alertTitle = getString(R.string.alertTitle);
+        String positiveButtonTitle = getString(R.string.positiveButtonTitle);
+
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+        EditText categoryEditText = new EditText(this);
+        categoryEditText.setInputType(InputType.TYPE_CLASS_TEXT);
+
+        alertBuilder.setTitle(alertTitle);
+        alertBuilder.setView(categoryEditText);
+
+        alertBuilder.setPositiveButton(positiveButtonTitle, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                dialogInterface.dismiss();
+
+            }
+        });
+
+        alertBuilder.create().show();
+
+    }
+
 }
